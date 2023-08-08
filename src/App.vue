e933d16e3447dbeac2a980c5a7017d133980991c
Boaz <boaserem022@gmail.com>
2023-08-08 14:56:58 +0300

    added hooks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer </h1>
+  <button @click="start"  :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Block.vue"
 export default {
+
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  components: {Block},
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
   }
+
 }
 </script>
 
@@ -23,4 +35,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+ button{
+  background: grey;
+  padding: 8px 4px;
+ }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="block" v-if="showBlock">
+        Click ME!
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["delay"],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        setTimeout(() =>{
+            this.showBlock = true;
+            console.log(this.delay)
+        },this.delay)
+    },
+    updated() {
+console.log("component updated")
+    },
+    unmounted(){
+
+    }
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: rgb(79, 235, 99);
+    color: #fff;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
